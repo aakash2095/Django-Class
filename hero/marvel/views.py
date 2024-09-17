0bbc51f91c66cd68d7c1e6f00accf5aab2423d95
Aakash Soni <soniaakash5336@gmail.com>
2024-09-17 18:15:58 +0530

    update

--- hero/marvel/views.py
+++ hero/marvel/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def marvel (request):
+def spiderman(request):
     return HttpResponse('i am spiderman')
+
+def ironman(request):
+    return HttpResponse('i am ironman')
+
+def antman(request):
+    return HttpResponse('i am antman')
