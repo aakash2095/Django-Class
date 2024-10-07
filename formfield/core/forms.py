71008af2c87e56853af9618d1c8209f52cda8707
Aakash Soni <soniaakash5336@gmail.com>
2024-10-07 16:15:53 +0530

    update

--- formfield/core/forms.py
+++ formfield/core/forms.py
@@ -4,3 +4,5 @@ from django import forms
 class Marvelform(forms.Form):
     # name = forms.CharField(label='Heroic Name',label_suffix=' - ',disabled=False,required=False,initial='Captain')
     name=forms.CharField(label='Heroic Name',widget=forms.TextInput(attrs={'class':'hn','placeholder':'Enter the name of hero'}))
+
+    
