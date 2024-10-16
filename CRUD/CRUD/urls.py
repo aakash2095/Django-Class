d4b9719f27ea15a88340289234637d1c85f7ec87
Aakash Soni <soniaakash5336@gmail.com>
2024-10-16 20:12:49 +0530

    update

--- CRUD/CRUD/urls.py
+++ CRUD/CRUD/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for crud_operation project.
+URL configuration for CRUD project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
