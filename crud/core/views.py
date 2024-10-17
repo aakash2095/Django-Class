00c7b33540e727338631c9a1f4537811b8ac1c1d
Aakash Soni <soniaakash5336@gmail.com>
2024-10-17 16:59:33 +0530

    update

--- crud/core/views.py
+++ crud/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Marvel
 from .forms import Marvelform
+from django.contrib import messages
 
 
 # Create your views here.
@@ -10,8 +11,27 @@ def base(request):
         mf=Marvelform(request.POST)
         if mf.is_valid():
             mf.save()
+            messages.success(request,'USER ADDED SUCCESSFULLY')
         return redirect('base')
     else:
         mf =Marvelform()
         mm = Marvel.objects.all()
         return render(request,'core/base.html',{'mf':mf,'marvel':mm})
+    
+
+def delete(request,id):
+    if request.method == 'POST':
+        mm=Marvel.objects.get(pk=id)
+        mm.delete()
+        return redirect('base')
+    
+def update(request,id):
+    if request.method == 'POST':
+        mm=Marvel.objects.get(pk=id)
+        mf = Marvelform(request.POST,instance=mm)
+        if mf.is_valid():
+            mf.save()
+    else:
+        mm=Marvel.objects.get(pk=id)
+        mf= Marvelform(instance=mm)
+    return render(request,'core/update.html',{'mf':mf})
