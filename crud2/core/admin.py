ebab87811deabfc83421f7b0e698cc20fdc51e2f
Aakash Soni <soniaakash5336@gmail.com>
2024-10-18 15:35:08 +0530

    update

---
+++ crud2/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Marvel
+# Register your models here.
+
+
+@admin.register(Marvel)
+class Marveladmin(admin.ModelAdmin):
+    list_display=['id','name','mail']
---
+++ crud2/core/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from .models import Marvel
+
+class MarvelForm(forms.ModelForm):
