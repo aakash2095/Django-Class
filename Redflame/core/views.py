941b5f15b3844d371d97b9317771a3d66d249ea3
Aakash Soni <soniaakash5336@gmail.com>
2024-10-31 15:45:39 +0530

    update

--- Redflame/core/views.py
+++ Redflame/core/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . forms import Registerform
 
 # Create your views here.
 
 def index(request):
     return render(request,'core/index.html')
+
+def register(request):
+    if request.method=="POST":
+        rf=Registerform(request.POST)
+        if rf.is_valid():
+            rf.save()
+            return redirect('register')
+    else:
+        rf=Registerform()
+        return render(request,'core/register.html',{'rf':rf})
+
